6d1194164425b1620e83bc987e5e8b38b2e58b78
Richa <richa.chaudhary30@gmail.com>
2025-12-29 02:16:05 +0530

    My opps project

--- opps_project.py
+++ opps_project.py
@@ -12,15 +12,61 @@ class chatbook:
                            3. Press 3 to write a post
                            4. Press 4 to message a friend
                            5. Press any other key to exit""")
+        print("\n")
         if user_input =="1":
-            pass
+            self.signup()
         elif user_input =="2":
-            pass
+            self.signin()
         elif user_input =="3":
-            pass
+            self.post_msg()
         elif user_input =="4":
-            pass
+            self.sent_msg()
         else:
             exit()
+    def signup(self):
+        email=input("Setup your email here: ")
+        pas =input("Setup Your Password here: ")
+        self.username=email
+        self.password=pas
+        print("You have signed up successfully")
+        print("\n")
+        self.menu()
+    def signin(self):
+        if self.username =='' and self.password =='':
+             print("You did not signup please signup first")
+        else:
+            username=input("Enter your user name: ")
+            pas=input("Enter your password: ")
+            if self.username ==username and self.password ==pas:
+                print("You have signedin successfully!!")
+                print("\n")
+                self.loggedin=True
+            else:
+                print("Please input correct credentials")
+        print("\n")
+        self.menu()
+    def post_msg(self):
+        if self.loggedin:
+            txt=input("Enter your post here: ")
+            print(f"Following content has been posted {txt}")
+        else:
+            print("Please sign in first")
+        print("\n")
+        self.menu()
+    def sent_msg(self):
+        if self.loggedin:
+            txt=input("Enter your message here: ")
+            name=input("Whom to sent the message: ")
+            print(f"Following message has been sent to {name} {txt}")
+        else:
+            print("Please sign in first")
+        print("\n")
+        self.menu()
+    
+    
+
+
+
+
 obj = chatbook()
     
